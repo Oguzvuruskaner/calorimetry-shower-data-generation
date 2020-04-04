980112a7432673e2971cb40a05c1863c54781212
oguzvuruskaner <oguzvuruskaner97@hotmail.com>
2020-04-05 00:37:44 +0300

    Main function for construct initial files for processing.

--- __main__.py
+++ __main__.py
@@ -1,19 +1,11 @@
-from detectAnomaly import getAbnormalValues
-import numpy as np
-from readRoot import create_all_quadruple_array_file
-from scripts import loadAndSplitArray
+from scripts import createScalers,createNpyFiles
 
-
-def main(args):
-    pass
+def main():
+    
+    createNpyFiles()
+    createScalers()
 
 
 if __name__ == "__main__":
 
-    create_all_quadruple_array_file(["pion50GeVshowers.root"])
-    loadAndSplitArray("npy/quadruple_all.npy",10)
-
-    # data = np.load("npy/quadruple_all_chunk_10.npy")
-    # data = np.array_split(data,90)[0]
-    # data.resize((data.shape[0],1))
-    # getAbnormalValues(data,model_path="isolation_forest_100.pkl")
+    main()
