75beb3f171ea630f5de5d57dec25f9f978343b56
oguzvuruskaner <oguzvuruskaner97@hotmail.com>
2020-04-05 01:32:14 +0300

    Changed scaler pickle files path organization.

--- preprocessors.py
+++ preprocessors.py
@@ -5,17 +5,17 @@ def train_preprocessors(data,dataset_name):
 
     standardScaler = StandardScaler()
     standardScaler.fit(data)
-    with open("scalers/standard_scaler_{}.pkl".format(dataset_name),"wb") as fp:
+    with open("scalers/{}_standard_scaler.pkl".format(dataset_name),"wb") as fp:
         dump(standardScaler,fp)
 
     minMaxScaler = MinMaxScaler()
     minMaxScaler.fit(data)
-    with open("scalers/min_max_scaler_{}.pkl".format(dataset_name), "wb") as fp:
+    with open("scalers/{}_min_max_scaler.pkl".format(dataset_name), "wb") as fp:
         dump(minMaxScaler, fp)
 
     robustScaler = RobustScaler()
     robustScaler.fit(data)
-    with open("scalers/robust_scaler_{}.pkl".format(dataset_name), "wb") as fp:
+    with open("scalers/{}_robust_scaler.pkl".format(dataset_name), "wb") as fp:
         dump(robustScaler, fp)
 
     maxAbsScaler = MaxAbsScaler()
