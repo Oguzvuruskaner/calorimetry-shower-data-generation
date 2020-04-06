464cd0f8230dd2e40d9955d9c4694dd36c375bbc
oguzvuruskaner <oguzvuruskaner97@hotmail.com>
2020-04-06 23:38:20 +0300

    Added sample scripts.

--- scripts.py
+++ scripts.py
@@ -118,3 +118,19 @@ def plotFeatures(NUMBER_OF_BINS=200,plot=False):
 
 
 
+def getSamples(sample_size=10**6):
+
+    for dataset_name in tqdm(__DATASETS__):
+
+        data = np.load(path.join("npy","{}.npy".format(dataset_name)),allow_pickle=True)
+
+        if len(data.shape) <= 2:
+            increment = data.shape[0]//sample_size
+            if increment == 0:
+                raise Exception("Size of sample : {} is bigger than dataset size {}.".format(sample_size,data.shape[0]))
+
+            else:
+                #Using systematic sampling to keep array distribution still.
+                data = np.sort(data)
+                np.save(path.join("npy","{}_{}.npy".format(dataset_name,sample_size)),data[::increment])
+
