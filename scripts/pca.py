e1e28e7c45b66673d0906c78e48f7d04e9beb42b
oguzvuruskaner <oguzvuruskaner97@hotmail.com>
2020-06-27 22:42:12 +0300

    Dumping PCA object and saving compressed array functionality added.

--- scripts/pca.py
+++ scripts/pca.py
@@ -1,10 +1,12 @@
 import os
 
-import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
+import pickle
 
 from sklearn.decomposition import PCA
+from config import N_COMPONENTS
+
 
 
 def plot_cum_pca():
@@ -15,13 +17,13 @@ def plot_cum_pca():
 
     data.resize((data.shape[0],data.shape[1] * data.shape[2]))
 
-    pca = PCA()
-
     # n_components <= min(num_samples,num_features)
     total_indices = min(data.shape[0],data.shape[1])
 
+    pca = PCA(total_indices)
 
-    pca.n_components = total_indices
+
+
     pca.fit(data)
 
     variance_array= np.cumsum(pca.explained_variance_ratio_)
@@ -36,3 +38,36 @@ def plot_cum_pca():
     plt.savefig(
         os.path.join("plots","pca_compression_jet_images.png")
     )
+
+# With respect to 1200 jet images data,
+# using 4-component PCA explains 0.99982353 variance of data
+# which is sufficient.
+
+def create_pca_compression():
+
+    pca = PCA(N_COMPONENTS)
+
+    data = np.load(
+        os.path.join("npy", "all_jet_images.npy")
+    )
+
+    data.resize((data.shape[0], data.shape[1] * data.shape[2]))
+
+
+
+    updated_data = pca.fit_transform(data)
+
+    np.save(
+        os.path.join("npy","{}_component_jet_images".format(N_COMPONENTS)),
+        updated_data
+    )
+
+    with open(
+        os.path.join("npy","{}_component_pca".format(N_COMPONENTS)),
+        "wb"
+    ) as fp:
+
+        pickle.dump(pca,fp)
+
+
+
