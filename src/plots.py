d082a90cefa32b55f9609ef777cba8d78b73ebb5
oguzvuruskaner <oguzvuruskaner97@hotmail.com>
2020-09-22 15:09:34 +0300

    Values smaller than plot error is going to be set to plot error to prevent any smaller values.

--- src/plots.py
+++ src/plots.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 import numpy as np
 
+from src.config import PLOT_ERROR
 
 
 def plot_data(data,title:str,ax=None):
@@ -15,10 +16,10 @@ def plot_data(data,title:str,ax=None):
 
     axes_image = ax.imshow(
         data * 50,
-        norm=colors.LogNorm(vmax=HIT_E_MAX),
+        norm=colors.LogNorm(vmin=PLOT_ERROR,vmax=HIT_E_MAX),
         extent=[HIT_Z_MIN,HIT_Z_MAX,HIT_R_MAX,0]
     )
-    axes_image.cmap.set_bad(color=axes_image.cmap(10e-20))
+    axes_image.cmap.set_bad(color=axes_image.cmap(PLOT_ERROR))
     ax.set_xlabel("Z")
     ax.set_ylabel("R")
     ax.set_title(title)
@@ -41,16 +42,19 @@ def plot_multiple_images(data,nrow : int):
     sup_figure.subplots_adjust(wspace=0.75,hspace=0.75)
     grid_spec = sup_figure.add_gridspec(nrow, ncolumn)
 
+
+    data[data < PLOT_ERROR] = PLOT_ERROR
+
     for i in range(nrow-1):
         for j in range(ncolumn):
             ax = sup_figure.add_subplot(grid_spec[i, j])
-            plot_data(data[i*ncolumn + j]+1e-40,counter,ax=ax)
+            plot_data(data[i*ncolumn + j],counter,ax=ax)
             counter += 1
             sup_figure.colorbar(ax.images[0])
 
     for i in range(remainder):
         ax = sup_figure.add_subplot(grid_spec[nrow-1, i])
-        plot_data(data[(nrow-1) * ncolumn + i]+1e-40, counter, ax=ax)
+        plot_data(data[(nrow-1) * ncolumn + i], counter, ax=ax)
         sup_figure.colorbar(ax.images[0])
 
     return sup_figure
