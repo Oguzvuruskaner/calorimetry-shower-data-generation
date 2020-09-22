25a0284b91e4bf044b3b59bd2dd70b3b1a8aad0b
oguzvuruskaner <oguzvuruskaner97@hotmail.com>
2020-09-22 14:22:44 +0300

    Added Swapout layer. Paper:https://arxiv.org/pdf/1605.06465.pdf

---
+++ src/models/Swapout.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as N
+from torch.distributions.bernoulli import Bernoulli
+
+class Swapout(N.Module):
+
+    def __init__(self, inner_layer : N.Module,f_x_survival_prob = 0.7,x_survival_prob = 0.8):
+        super().__init__()
+
+        self._inner_layer = inner_layer
+        self.f_x_bernoulli = Bernoulli(f_x_survival_prob)
+        self.x_bernoulli = Bernoulli(x_survival_prob)
+
+
+    def forward(self,x):
+
+        module_device = self._inner_layer.get_device()
+        x_survival_matrix = self.x_bernoulli.sample(x.shape).to(module_device)
+        f_x_survival_matrix = self.f_x_bernoulli.sample(x.shape).to(module_device)
+
+        return x * x_survival_matrix + f_x_survival_matrix * self._inner_layer(x)
