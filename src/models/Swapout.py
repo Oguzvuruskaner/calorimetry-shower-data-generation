da87c89240f4c609f5569999280ddfa64146ac2f
oguzvuruskaner <oguzvuruskaner97@hotmail.com>
2020-09-25 10:34:58 +0300

    Swapout model is optimized with inner Dropout layers.

--- src/models/Swapout.py
+++ src/models/Swapout.py
@@ -1,6 +1,4 @@
-import torch
 import torch.nn as N
-from torch.distributions.bernoulli import Bernoulli
 
 class Swapout(N.Module):
 
@@ -8,17 +6,11 @@ class Swapout(N.Module):
         super().__init__()
 
         self._inner_layer = inner_layer
-        self.f_x_bernoulli = Bernoulli(f_x_survival_prob)
-        self.x_bernoulli = Bernoulli(x_survival_prob)
+        self.f_x_dropout = N.Dropout(1-f_x_survival_prob)
+        self.x_dropout = N.Dropout(1-x_survival_prob)
 
 
     def forward(self,x):
 
-        if self.training:
-            x_survival_matrix = self.x_bernoulli.sample(x.shape).to(x.device)
-            f_x_survival_matrix = self.f_x_bernoulli.sample(x.shape).to(x.device)
-
-            return x * x_survival_matrix + f_x_survival_matrix * self._inner_layer(x)
-
-        else:
-            return x + self._inner_layer(x)
+        f_x = self._inner_layer(x)
+        return self.x_dropout(x) + self.f_x_dropout(f_x)
