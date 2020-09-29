3212f581cfaf3b9b7002d40ed96e07518d2d25b8
oguzvuruskaner <oguzvuruskaner97@hotmail.com>
2020-09-29 16:57:22 +0300

    Default survival probability reduced to .5

--- src/layers/BottleneckSwapout.py
+++ src/layers/BottleneckSwapout.py
@@ -5,11 +5,11 @@ from src.layers.BottleneckLayer import BottleneckLayer
 
 class BottleneckSwapout(BottleneckLayer):
 
-    def __init__(self, in_channels,out_channels,f_x_survival_prob = 0.7,x_survival_prob = 0.8,relu=False):
+    def __init__(self, in_channels,out_channels,f_x_survival_prob = 0.5,x_survival_prob = 0.5,relu=False):
         super().__init__(in_channels,out_channels,relu = relu)
 
         self.f_x_dropout = N.Dropout(1-f_x_survival_prob)
-        self.x_dropout = N.Dropout(1-x_survival_prob)
+        self.x_dropout = N.Dropout(1-x_survival_prob )
 
 
     def forward(self,x):
